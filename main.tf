aaa386b0ef35446fe133e9ed920b5e260816b8d0
Mukesh Sirvi <160573038+mukeshsirvi18@users.noreply.github.com>
2025-05-02 15:47:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "mukesh-s3-backend"
+    key            = "terraform.tfstate"
+    region         = "ca-central-1"
+  }
+}
 provider "aws" {
   region = var.aws_region
 }
